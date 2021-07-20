1dc86c9f6caba30773a8f51b1f14262de3950604
Somayeh Bahiraie <somayebhr@gmail.com>
2021-07-20 18:16:09 +0430

    feat: create home and navbar component

---
+++ src/cpomponents/Home.js
@@ -0,0 +1,9 @@
+export const Home = () => {
+	const handleClick = () => {};
+	return (
+		<div className="home">
+			<h2>Homepage</h2>
+			<button onClick={handleClick}>Click me</button>
+		</div>
+	);
+};
