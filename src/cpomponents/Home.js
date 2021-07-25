7ec1b424ce8331261034ae16f25f6ba5afe65de3
Somayeh Bahiraie <somayebhr@gmail.com>
2021-07-25 11:24:11 +0430

    feat: props and hooks

--- src/cpomponents/Home.js
+++ src/cpomponents/Home.js
@@ -1,9 +1,20 @@
+import { useState, useEffect } from "react";
+import { BlogList } from "./BlogList";
+
 export const Home = () => {
-	const handleClick = () => {};
+	const [blogs, setBlogs] = useState([
+		{ title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+		{ title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+		{ title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3 },
+	]);
+	const handleDelete = (id) => {
+		const newBlogs = blogs.filter((blog) => blog.id !== id);
+		setBlogs(newBlogs);
+	};
+	useEffect(() => {});
 	return (
 		<div className="home">
-			<h2>Homepage</h2>
-			<button onClick={handleClick}>Click me</button>
+			<BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} />
 		</div>
 	);
 };
