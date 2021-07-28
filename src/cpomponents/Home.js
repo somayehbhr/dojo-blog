8972048241b492a8324899f682637e477ff67522
Somayeh Bahiraie <somayebhr@gmail.com>
2021-07-28 15:25:43 +0430

    feat: add json server

--- src/cpomponents/Home.js
+++ src/cpomponents/Home.js
@@ -2,19 +2,38 @@ import { useState, useEffect } from "react";
 import { BlogList } from "./BlogList";
 
 export const Home = () => {
-	const [blogs, setBlogs] = useState([
-		{ title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-		{ title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-		{ title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3 },
-	]);
+	const [blogs, setBlogs] = useState(null);
+	const [isPending, setIspending] = useState(true);
+	const [error, setError] = useState(null);
 	const handleDelete = (id) => {
 		const newBlogs = blogs.filter((blog) => blog.id !== id);
 		setBlogs(newBlogs);
 	};
-	useEffect(() => {});
+	useEffect(() => {
+		setTimeout(() => {
+			fetch("http://localhost:8000/blogs")
+				.then((res) => {
+					if (!res.ok) {
+						throw Error("could not fetch the data for that resource");
+					}
+					return res.json();
+				})
+				.then((data) => {
+					setBlogs(data);
+					setIspending(false);
+					setError(null);
+				})
+				.catch((e) => {
+					setIspending(false);
+					setError(e.message);
+				});
+		}, 1000);
+	}, []);
 	return (
 		<div className="home">
-			<BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} />
+			{error && <div>{error}</div>}
+			{isPending && <div>Loading...</div>}
+			{blogs && <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} />}
 		</div>
 	);
 };
